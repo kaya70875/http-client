09b698c36ec66da6c57f89fe5e314fda1f88f7f0
Ahmet Kaya <shortsforturkey@gmail.com>
2025-06-13 17:54:37 +0300

    add error handling to processResponse function

--- utils/processResponse.ts
+++ utils/processResponse.ts
@@ -1,25 +1,34 @@
 import { ResponseT } from "../types/clientTypes";
 
 export function processResponse(raw: string): ResponseT {
-  const [headerPart, dataSegment] = raw.split("\r\n\r\n");
+  try {
+    const [headerPart, dataSegment] = raw.split("\r\n\r\n");
 
-  const headerLines = headerPart.split("\r\n");
-  const statusLine = headerLines[0]; // HTTP/1.1 200 OK
+    const headerLines = headerPart.split("\r\n");
+    const statusLine = headerLines[0]; // HTTP/1.1 200 OK
 
-  const [_, statusCode, statusMessage] =
-    statusLine.match(/HTTP\/\d\.\d (\d{3}) (.*)/) || [];
+    const [_, statusCode, statusMessage] =
+      statusLine.match(/HTTP\/\d\.\d (\d{3}) (.*)/) || [];
 
-  const headers: Record<string, string> = {};
-  for (let i = 1; i < headerLines.length; i++) {
-    const [key, ...rest] = headerLines[i].split(":");
-    headers[key.trim()] = rest.join(":").trim();
+    const headers: Record<string, string> = {};
+    for (let i = 1; i < headerLines.length; i++) {
+      const [key, ...rest] = headerLines[i].split(":");
+      headers[key.trim()] = rest.join(":").trim();
+    }
+
+    console.log("data", dataSegment);
+    const data = JSON.parse(dataSegment);
+
+    return {
+      statusCode: Number(statusCode),
+      statusMessage,
+      data,
+    };
+  } catch (err) {
+    return {
+      statusCode: Number(500),
+      statusMessage: `error while processing raw data: ${err}`,
+      data: null,
+    };
   }
-
-  const data = JSON.parse(dataSegment);
-
-  return {
-    statusCode: Number(statusCode),
-    statusMessage,
-    data,
-  };
 }
