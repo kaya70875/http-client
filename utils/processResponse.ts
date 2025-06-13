23aa453758fb150705732f32286ce74e444de9e7
Ahmet Kaya <shortsforturkey@gmail.com>
2025-06-13 15:52:58 +0300

    create utility function for processing raw server response

---
+++ utils/processResponse.ts
@@ -0,0 +1,25 @@
+import { ResponseT } from "../types/clientTypes";
+
+export function processResponse(raw: string): ResponseT {
+  const [headerPart, dataSegment] = raw.split("\r\n\r\n");
+
+  const headerLines = headerPart.split("\r\n");
+  const statusLine = headerLines[0]; // HTTP/1.1 200 OK
+
+  const [_, statusCode, statusMessage] =
+    statusLine.match(/HTTP\/\d\.\d (\d{3}) (.*)/) || [];
+
+  const headers: Record<string, string> = {};
+  for (let i = 1; i < headerLines.length; i++) {
+    const [key, ...rest] = headerLines[i].split(":");
+    headers[key.trim()] = rest.join(":").trim();
+  }
+
+  const data = JSON.parse(dataSegment);
+
+  return {
+    statusCode: Number(statusCode),
+    statusMessage,
+    data,
+  };
+}
