4357b30750622124e15a03a8dc6a1bc0a4e8ec63
Ahmet Kaya <shortsforturkey@gmail.com>
2025-06-13 15:52:29 +0300

    create custom http client with net module

---
+++ client/CustomClient.ts
@@ -0,0 +1,70 @@
+import * as net from "net";
+import { RequestConfig, ResponseT } from "../types/clientTypes";
+import { processResponse } from "../utils/processResponse";
+
+export class CustomClient {
+  port: number;
+
+  constructor(port: number = 80) {
+    this.port = port;
+  }
+
+  get(url: string, config?: RequestConfig): Promise<ResponseT> {
+    return new Promise((resolve, reject) => {
+      const parsedUrl = new URL(url);
+      const host = parsedUrl.host;
+      const path = parsedUrl.pathname + parsedUrl.search;
+
+      const defaultHeaders: Record<string, string> = {
+        "Content-Type": "application/json",
+      };
+
+      const mergedHeaders = {
+        ...defaultHeaders,
+        ...(config?.headers ?? {}),
+      };
+
+      const headerString = Object.entries(mergedHeaders)
+        .map((key, value) => `${key}: ${value}\r\n`)
+        .join("");
+
+      const client = net.createConnection({ host, port: this.port }, () => {
+        const request =
+          `GET ${path} HTTP/1.1\r\n` +
+          `Host: ${host}\r\n` +
+          headerString +
+          `Connection: close\r\n` +
+          `\r\n`;
+
+        client.write(request);
+      });
+
+      // Set timeout for request.
+      client.setTimeout(16000);
+
+      let response = "";
+
+      client.on("data", (chunk) => {
+        response += chunk.toString();
+      });
+
+      client.on("end", () => {
+        const processed = processResponse(response);
+        resolve(processed);
+      });
+
+      client.on("timeout", () => {
+        client.destroy();
+        reject(new Error("Request timed out."));
+      });
+
+      client.on("error", (err) => {
+        reject(err);
+      });
+    });
+  }
+
+  post(url: string, config: RequestConfig) {
+    // Can be implemented later
+  }
+}
