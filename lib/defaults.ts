e8b704dfa7ef94d06ed57cc69d76f37b26ee8218
Ahmet Kaya <shortsforturkey@gmail.com>
2025-06-14 15:07:05 +0300

    create defaults

---
+++ lib/defaults.ts
@@ -0,0 +1,26 @@
+export const defaults = {
+  headers: {
+    common: {
+      Accept: "application/json",
+    },
+    get: {
+      "Content-Type": "application/json",
+    },
+    post: {
+      "Content-Type": "application/json",
+      "X-Testing": "test/value",
+    },
+  },
+  port: 80,
+  timeout: 16000,
+};
+
+export interface DefaultConfig {
+  headers: {
+    common: Record<string, string>;
+    get: Record<string, string>;
+    post: Record<string, string>;
+  };
+  port: number;
+  timeout: number;
+}
